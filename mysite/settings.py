050ce48f38e99cba9b91d91834208bccd3809270
Basyrov Rustam <hrustbas@gmail.com>
2025-06-05 01:38:43 +0300

    fixed allowed hosts
    Without it, Django can't work in docker-network

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-7@a)l@%smekjdwtc%$)k=qs^5k8@fnmc+vxk)(!mi5ojrbhgn+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
